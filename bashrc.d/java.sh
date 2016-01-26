7f5c186fdd50695e6c511a6af622b4e502721030
Jérôme Mainaud <jmainaud@ippon.fr>
2016-01-26 18:28:46 +0100

    Gestion des erreurs lorsque le jdk demandé n'existe pas.

--- bashrc.d/java.sh
+++ bashrc.d/java.sh
@@ -2,17 +2,30 @@
 
 function __jmt_cmd_java() {
     local version=$1
-    local java_home=/usr/libexec/java_home
+    local java_home_cmd=/usr/libexec/java_home
+    local java_home
 
     case "$version" in
         -l)
-            $java_home -V
+            $java_home_cmd -V
             ;;
         6|7|8)
-            export JAVA_HOME=$($java_home -v 1.$version)
+            if java_home=$($java_home_cmd -v 1.$version 2>/dev/null); then
+                export JAVA_HOME=$java_home
+            elif java_home=$($java_home_cmd -v $version 2>/dev/null); then
+                export JAVA_HOME=$java_home
+            else
+                echo "Il n'y a pas de JDK en version $version" >&2
+                return 1
+            fi
             ;;
         *)
-            export JAVA_HOME=$($java_home -v $version)
+            if java_home=$($java_home_cmd -v $version 2>/dev/null); then
+                export JAVA_HOME=$java_home
+            else
+                echo "Il n'y a pas de JDK en version $version" >&2
+                return 1
+            fi
             ;;
     esac
 }
